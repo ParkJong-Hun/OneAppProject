6bda575c66b38343751660287596ae804435d818
ParkJong-Hun <thesn515@gmail.com>
2022-04-29 14:20:09 +0900

    feat:initinalize ios app

---
+++ ios/one-app-project-ios/one-app-project-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  one-app-project-ios
+//
+//  Created by 박종훈 on 2022/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
