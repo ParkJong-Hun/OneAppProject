ed61c4c187876807144b36703e89763b2f638383
ParkJong-Hun <thesn515@gmail.com>
2022-04-29 14:28:50 +0900

    feat:initialize api

---
+++ api/one-app-project-api/src/main/kotlin/com/example/oneappproejct/api/oneappprojectapi/OneAppProjectApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.oneappproejct.api.oneappprojectapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OneAppProjectApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<OneAppProjectApiApplication>(*args)
+}
---
+++ api/one-app-project-api/src/test/kotlin/com/example/oneappproejct/api/oneappprojectapi/OneAppProjectApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.oneappproejct.api.oneappprojectapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OneAppProjectApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
