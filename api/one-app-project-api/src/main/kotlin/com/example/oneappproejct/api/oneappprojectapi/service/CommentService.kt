fc99a629d5d5b34eb66120e30f079a0b114f5ec8
ParkJong-Hun <thesn515@gmail.com>
2022-05-01 14:49:41 +0900

    feat:edit Service scaffolding

--- api/one-app-project-api/src/main/kotlin/com/example/oneappproejct/api/oneappprojectapi/service/CommentService.kt
+++ api/one-app-project-api/src/main/kotlin/com/example/oneappproejct/api/oneappprojectapi/service/CommentService.kt
@@ -1,4 +1,26 @@
 package com.example.oneappproejct.api.oneappprojectapi.service
 
+import com.example.oneappproejct.api.oneappprojectapi.dto.CommentRequestDto
+import com.example.oneappproejct.api.oneappprojectapi.dto.CommentResponseDto
+
 class CommentService {
+    fun createComment(id: Long, body: CommentRequestDto): CommentResponseDto {
+        return CommentResponseDto()
+    }
+
+    fun readAllCommentOnPost(id: Long): List<CommentResponseDto> {
+        return listOf()
+    }
+
+    fun readComment(postId: Long, commentId: Long): CommentResponseDto {
+        return CommentResponseDto()
+    }
+
+    fun updateComment(postId: Long, commentId: Long, body: CommentRequestDto): CommentResponseDto {
+        return CommentResponseDto()
+    }
+
+    fun deleteComment(postId: Long, commentId: Long): CommentResponseDto {
+        return CommentResponseDto()
+    }
 }
--- api/one-app-project-api/src/main/kotlin/com/example/oneappproejct/api/oneappprojectapi/service/PostService.kt
+++ api/one-app-project-api/src/main/kotlin/com/example/oneappproejct/api/oneappprojectapi/service/PostService.kt
@@ -1,7 +1,28 @@
 package com.example.oneappproejct.api.oneappprojectapi.service
 
+import com.example.oneappproejct.api.oneappprojectapi.dto.PostRequestDto
+import com.example.oneappproejct.api.oneappprojectapi.dto.PostResponseDto
 import org.springframework.stereotype.Service
 
 @Service
 class PostService {
+    fun createPost(body: PostRequestDto): PostResponseDto {
+        return PostResponseDto()
+    }
+
+    fun readAllPost(): List<PostResponseDto> {
+        return listOf()
+    }
+
+    fun readPost(id: Long): PostResponseDto {
+        return PostResponseDto()
+    }
+
+    fun updatePost(id: Long, body: PostRequestDto): PostResponseDto {
+        return PostResponseDto()
+    }
+
+    fun deletePost(id: Long): PostResponseDto {
+        return PostResponseDto()
+    }
 }
